2637550ae30c685e7581093b933581689e9fd491
sun9zzang <sun9nyy@gmail.com>
2022-09-06 11:58:42 +0900

    with csv DB

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from uuid import uuid4
+import pandas as pd
 
 
 app = FastAPI()
-
-current_tasks = []
 
 
 class Task(BaseModel):
@@ -17,21 +16,27 @@ class Task(BaseModel):
 
 @app.get("/tasks/")
 async def get_tasks():
-    return {"current_task": current_tasks}
+    try:
+        current_tasks_df = pd.read_csv("DB_task.csv", index_col=0)
+    except pd.errors.EmptyDataError:
+        return {"current_task": ""}
+    else:
+        return {"current_task": current_tasks_df.to_json(orient="records")}
 
 
 @app.post("/tasks/")
 async def add_task(task: Task):
     task.uid = str(uuid4())
-    print(task)
-    current_tasks.append(task)
+    task_df = pd.DataFrame.from_records([{"uid": task.uid, "title": task.title, "content": task.content, "deadline": task.deadline}])
+    task_df.to_csv("DB_task.csv", mode="a", header=False, index=False)
     return {"msg": "Task added successfully!", "title_task_added": task.title}
 
 
 @app.delete("/tasks/{task_uid}")
 async def delete_task(task_uid: str):
-    for i, task in enumerate(current_tasks):
-        if task.uid == task_uid:
-            del current_tasks[i]
-            return {"task_del_message": "Task deleted successfully!", "uid_task_deleted": task_uid}
+    current_tasks_df = pd.read_csv("DB_task.csv", index_col=0)
+    print(current_tasks_df.to_string())
+    current_tasks_df = current_tasks_df.loc[current_tasks_df.index != task_uid]
+    current_tasks_df.to_csv("DB_task.csv")
+    return {"task_del_message": "Task deleted successfully!", "uid_task_deleted": task_uid}
     
